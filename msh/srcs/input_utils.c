ae1ff1a176d4b41fb67d1aadb848b787d1bdcf54
iW90 <ingrid.wagner.dev@gmail.com>
2023-08-26 14:28:15 -0300

    v2.1

---
+++ msh/srcs/input_utils.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 22:20:34 by inwagner          #+#    #+#             */
+/*   Updated: 2023/08/19 13:44:42 by inwagner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*	PEGA O TRECHO ENTRE QUOTES
+** Enquanto a quote estiver aberta, o restante do código
+** não será interpretado, considerando como parte do
+** conteúdo quotado.
+** Sai do programa se não fechar o quote.
+*/
+void	get_quote(char *input, int *i)
+{
+	char	quote;
+
+	quote = input[(*i)++];
+	while (input[*i] && input[*i] != quote)
+		(*i)++;
+}
+
+int	is_bracket(char c)
+{
+	return (c == '>' || c == '<');
+}
+
+int	is_pipe(char c)
+{
+	return (c == '|');
+}
---
+++ msh/srcs/input_validator.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_validator.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/05 19:38:31 by inwagner          #+#    #+#             */
+/*   Updated: 2023/08/21 18:07:05 by inwagner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	print_error(char *msg, char *refstr, char refchar)
+{
+	ft_putstr_fd("msh: ", STDERR_FILENO);
+	ft_putstr_fd(msg, STDERR_FILENO);
+	if (refstr)
+		ft_putstr_fd(refstr, STDERR_FILENO);
+	if (refchar)
+		ft_putchar_fd(refchar, STDERR_FILENO);
+	if (refstr || refchar)
+		ft_putstr_fd("'", STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	return (1);
+}
+
+static int	check_unclosed_quotes(char *input, int *i)
+{
+	char	quote;
+
+	quote = input[*i];
+	get_quote(input, i);
+	if (input[*i])
+		return (0);
+	print_error("unable to find matching `", NULL, quote);
+	return (-1);
+}
+
+static int	validate_pipe(char *input, int *i)
+{
+	if (is_bracket(input[*i + 1]))
+		(*i)++;
+	if (input[*i + 1] == input[*i] && !is_pipe(input[*i]))
+		(*i)++;
+	(*i)++;
+	while (input[*i] && ft_isblank(input[*i]))
+		(*i)++;
+	if (is_pipe(input[*i]) || is_bracket(input[*i]))
+		return (print_error("syntax error near unexpected token `", \
+				NULL, input[*i]));
+	(*i)--;
+	return (0);
+}
+
+static int	validate_brackets(char *input, int *i)
+{
+	if (input[*i + 1] == input[*i])
+		(*i)++;
+	(*i)++;
+	while (input[*i] && ft_isblank(input[*i]))
+		(*i)++;
+	if (!input[*i])
+		return (print_error("syntax error near unexpected token `", \
+				"newline", 0));
+	if (is_pipe(input[*i]) || is_bracket(input[*i]))
+		return (print_error("syntax error near unexpected token `", \
+				NULL, input[*i]));
+	(*i)--;
+	return (0);
+}
+
+int	validate_input(char *input)
+{
+	int	i;
+
+	if (!input)
+		return (0);
+	i = -1;
+	while (input[++i])
+	{
+		if (is_quote(input[i]))
+			if (check_unclosed_quotes(input, &i))
+				return (-1);
+		if (input[i] && is_pipe(input[i]))
+			if (validate_pipe(input, &i))
+				return (-1);
+		if (input[i] && is_bracket(input[i]))
+			if (validate_brackets(input, &i))
+				return (-1);
+	}
+	return (0);
+}
