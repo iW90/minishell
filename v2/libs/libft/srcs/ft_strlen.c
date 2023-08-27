1abe52c9d9d39f25abc1f846f1d687a52d0b14a1
iW90 <ingrid.wagner.dev@gmail.com>
2023-08-27 10:56:27 -0300

    feat: add NULL validation

--- v2/libs/libft/srcs/ft_strlen.c
+++ v2/libs/libft/srcs/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 19:09:57 by inwagner          #+#    #+#             */
-/*   Updated: 2023/06/09 11:31:43 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/08/27 10:55:05 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *src)
 {
 	size_t	len;
 
+	if (!src)
+		return (0);
 	len = 0;
 	while (src[len])
 		len++;
