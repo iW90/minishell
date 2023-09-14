a69e62169d87348d16a0b0814257bffbd53f8a08
iW90 <ingrid.wagner.dev@gmail.com>
2023-09-14 19:51:05 -0300

    feat: add pipe validation

--- v2/srcs/input_utils.c
+++ v2/srcs/input_utils.c
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/21 22:20:34 by inwagner          #+#    #+#             */
-/*   Updated: 2023/09/08 09:37:14 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/09/14 19:28:40 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,18 @@ int	is_bracket(char c)
 int	is_pipe(char c)
 {
 	return (c == '|');
+}
+
+int	print_error(char *msg, char *refstr, char refchar)
+{
+	ft_putstr_fd("msh: ", STDERR_FILENO);
+	ft_putstr_fd(msg, STDERR_FILENO);
+	if (refstr)
+		ft_putstr_fd(refstr, STDERR_FILENO);
+	if (refchar)
+		ft_putchar_fd(refchar, STDERR_FILENO);
+	if (refstr || refchar)
+		ft_putstr_fd("'", STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	return (1);
 }
